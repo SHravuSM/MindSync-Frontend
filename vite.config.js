1a956888957590c009505c28a59f41814642eac5
SHravankumar Muchchandi <shravankumarmuchchandi@gmail.com>
2025-04-21 17:24:11 +0530

    21-04-2025

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), visualizer()],
-  build: {
-    chunkSizeWarningLimit: 1000, // increase limit to 1000KB (1MB)
-  },
+  plugins: [react(), tailwindcss()],
 });
